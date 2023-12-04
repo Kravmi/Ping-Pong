5d5eba3af20fc400086567f0615711583fa58eb2
Kravmi <144707588+Kravmi@users.noreply.github.com>
2023-12-04 16:57:03 +0300

    Доделали игру!!! Теперь можно нормально без багов управлять ракетками, а также мячик прекрасно перемещается при отталкивании. Есть условия завершения игры.

--- Ping-Pong.py
+++ Ping-Pong.py
@@ -18,33 +18,58 @@ class Player(GameSprite):
     def update_left(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_w] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[pg.K_s] and self.rect.y < 620:
-            self.rect.x += self.speed
+            self.rect.y -= self.speed
+        if keys[pg.K_s] and self.rect.y < 350:
+            self.rect.y += self.speed
 
     def update_right(self):
         keys = pg.key.get_pressed()
         if keys[pg.K_UP] and self.rect.y > 5:
-            self.rect.x -= self.speed
-        if keys[pg.K_DOWN] and self.rect.y < 620:
-            self.rect.x += self.speed
+            self.rect.y -= self.speed
+        if keys[pg.K_DOWN] and self.rect.y < 350:
+            self.rect.y += self.speed
 
 
 pg.init()
 window = pg.display.set_mode((600, 500))
 pg.display.set_caption('Ping-Pong')
 game = True
-racket_left = Player('racket.png', 50, 150, 30, 20, 5)
-racket_right = Player('racket.png', 50, 150, 520, 200, 5)
-ball = GameSprite('tenis_ball.png', 50, 50, 200, 200, 7)
+speed_x = 1
+speed_y = 2
+finish = False
+pg.font.init()
+font = pg.font.Font(None, 34)
+win_left = font.render('Левый игрок выиграл!', True, (0, 0, 0))
+win_right = font.render('Правый игрок выиграл!', True, (0, 0, 0))
+background = pg.image.load('Background.png')
+racket_left = Player('racket.png', 50, 150, 30, 200, 2)
+racket_right = Player('racket.png', 50, 150, 520, 200, 2)
+ball = GameSprite('tenis_ball.png', 50, 50, 275, 230, 1)
 
 while game:
     for i in pg.event.get():
         if i.type == pg.QUIT:
             game = False
-    racket_left.reset()
-    racket_right.reset()
-    ball.reset()
-    racket_left.update_left()
-    racket_right.update_right()
+    if not finish:
+        window.blit(background, (0, 0))
+        racket_left.reset()
+        racket_right.reset()
+        ball.reset()
+        racket_left.update_left()
+        racket_right.update_right()
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if pg.sprite.collide_rect(ball, racket_left) or pg.sprite.collide_rect(ball, racket_right):
+            speed_x *= -1
+            speed_y *= 1
+        if ball.rect.y == 440 or ball.rect.y < 20:
+            speed_y *= -1
+            speed_x *= 1
+        if ball.rect.x <= -30:
+            finish = True
+            window.blit(win_right, (200, 200))
+        if ball.rect.x >= 590:
+            finish = True
+            window.blit(win_left, (200, 200))
+
     pg.display.update()
